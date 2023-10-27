3eeba7943d9dcd0091b8a449c4948e734ff9dd22
sadhvibs <bssadhvi1999@gmail.com>
2023-10-27 17:24:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// window.addEventListener('load', function(){
+//     new Glider(document.querySelector('.glider'), {
+//       setting-name: setting-value
+//     })
+//   })
+var flkty = new Flickity('.carousel', {
+  wrapAround: true
+});
